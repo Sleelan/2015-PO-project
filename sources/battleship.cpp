e7723b5fc223e1f1b4bf1640c9f8c494ff4f414c
Sleelan <sleelanzolw@gmail.com>
2015-06-05 17:22:14 +0200

    Create battleship.cpp

---
+++ sources/battleship.cpp
@@ -0,0 +1,47 @@
+#include "battleship.h"
+
+battleship::battleship()
+{
+    *this->health = 80;
+    *this->maxHealth = 80;
+    *this->shields = 4;
+    *this->maxShields = 4;
+    *this->weaponPower = 8;
+    *this->accuracy = 4;
+    *this->pierce = 0;
+    *this->enginesLevel = 2;
+    *this->armorLevel = 3;
+    *this->damagedEngines = false;
+    *this->damagedWeapons = false;
+    *this->damagedOptics = false;
+    *this->breachedHull = false;
+    *this->damagedShields = false;
+    originalShields = new int;
+    originalMaxShields = new int;
+    originalWeaponPower = new int;
+}
+battleship::~battleship()
+{
+    delete originalShields;
+    delete originalMaxShields;
+    delete originalWeaponPower;
+}
+
+void battleship::specialPowerOn()
+{
+    *this->originalShields = *this->shields;
+    *this->originalMaxShields = *this->maxShields;
+    *this->originalWeaponPower = *this->weaponPower;
+    *this->shields = *this->shields * 2;
+    *this->maxShields = *this->maxShields * 2;
+    *this->weaponPower = (int)(*this->weaponPower * 0.5);
+    *flagSpecialPowerOn = true;
+}
+
+void battleship::specialPowerOff()
+{
+    *this->shields = *this->shields / 2;
+    *this->maxShields = *this->originalMaxShields;
+    *this->weaponPower = *this->originalWeaponPower;
+    *flagSpecialPowerOn = false;
+}
