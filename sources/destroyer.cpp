95b5bbd9cd15f852cd3eedd5d017573841732c74
Sleelan <sleelanzolw@gmail.com>
2015-06-05 17:23:12 +0200

    Create destroyer.cpp

---
+++ sources/destroyer.cpp
@@ -0,0 +1,42 @@
+#include "destroyer.h"
+
+destroyer::destroyer()
+{
+    *this->health = 40;
+    *this->maxHealth = 40;
+    *this->shields = 2;
+    *this->maxShields = 2;
+    *this->weaponPower = 4;
+    *this->accuracy = 6;
+    *this->pierce = 0;
+    *this->enginesLevel = 6;
+    *this->armorLevel = 0;
+    *this->damagedEngines = false;
+    *this->damagedWeapons = false;
+    *this->damagedOptics = false;
+    *this->breachedHull = false;
+    *this->damagedShields = false;
+    originalEnginesLevel = new int;
+    originalAccuracy = new int;
+}
+destroyer::~destroyer()
+{
+    delete originalEnginesLevel;
+    delete originalAccuracy;
+}
+
+void destroyer::specialPowerOn()
+{
+    *this->originalEnginesLevel = *this->shields;
+    *this->originalAccuracy = *this->accuracy;
+    *this->enginesLevel = *this->enginesLevel * 2;
+    *this->accuracy = *this->accuracy / 2;
+    *flagSpecialPowerOn = true;
+}
+
+void destroyer::specialPowerOff()
+{
+    *this->enginesLevel = *this->originalEnginesLevel;
+    *this->accuracy = *this->originalAccuracy;
+    *flagSpecialPowerOn = false;
+}
